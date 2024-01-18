b17b10d29d3e975bb206ad79d0691c60951161e8
maharathi31 <hriday.rathi@plaksha.edu.in>
2024-01-18 21:36:02 +0530

    bottom bar functionality completed

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,15 @@ import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import Home from "./_root/pages/Home";
 import RootLayout from "./_root/RootLayout";
+import Explore from "./_root/pages/Explore";
+import Saved from "./_root/pages/Saved";
+import AllUsers from "./_root/pages/AllUsers";
+import CreatePost from "./_root/pages/CreatePost";
+import EditPost from "./_root/pages/EditPost";
+import PostDetails from "./_root/pages/PostDetails";
+import Profile from "./_root/pages/Profile";
+import UpdateProfile from "./_root/pages/UpdateProfile";
+
 const App = () => {
   return <div className="flex h-screen">
    <Routes>
@@ -18,6 +27,14 @@ const App = () => {
      {/* private routes */}
      <Route element={<RootLayout/>}>
      <Route index element={<Home/>}/>
+     <Route path="/explore" element={<Explore/>}/>
+     <Route path="/saved" element={<Saved/>}/>
+     <Route path="/all-users" element={<AllUsers/>}/>
+     <Route path="/create-post" element={<CreatePost/>}/>
+     <Route path="/update-post/:id" element={<EditPost/>}/>
+     <Route path="/posts/:id" element={<PostDetails/>}/>
+     <Route path="/profile/:id/*" element={<Profile/>}/>
+     <Route path="/update-profile/:id" element={<UpdateProfile/>}/>
      </Route>
     </Routes>
     <Toaster/>
