ee613fa7a1cfef2dffdc103dd4a45583d24f863e
maharathi31 <hriday.rathi@plaksha.edu.in>
2024-01-19 17:39:37 +0530

    file uploader functionality in progress

---
+++ src/components/forms/PostForm.tsx
@@ -0,0 +1,106 @@
+import * as z from "zod";
+import { Models } from "appwrite";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  })
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+  } from "../ui/form";
+  import { Textarea } from "../ui/textarea";
+  import { Button } from "../ui/button";
+  import {Input} from "../ui/input"
+import FileUploader from "../ui/shared/FileUploader";
+
+const PostForm = ({post}) => {
+     // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+        <FormField
+          control={form.control}
+          name="caption"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">caption</FormLabel>
+              <FormControl>
+                <Textarea placeholder="shadcn" {...field} className="shad-textarea custom-scrollbar"/>
+              </FormControl>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Photos</FormLabel>
+              <FormControl>
+                <FileUploader fieldChange={field.onChange} mediaUrl={post?.imageUrl}/>
+              </FormControl>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Location</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input"/>
+              </FormControl>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Tags
+              (separated by comma ",")</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" placeholder="Art,Expression,Learn"/>
+              </FormControl>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-4 items-center justify-end">
+        <Button type="button" className="shad-button_dark_4">Cancel</Button>
+        <Button type="submit" className="shad-button_primary whitespace-nowrap">Submit</Button>
+        </div>
+      </form>
+    </Form>
+  )
+};
+
+export default PostForm;
